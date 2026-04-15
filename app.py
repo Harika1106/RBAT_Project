87493b764f500a2fac5fc893277e9f575df16d38
Harika <harikavasamsetti2004@gmail.com>
2026-04-15 23:06:22 +0530

    final fix

--- app.py
+++ app.py
@@ -424,5 +424,5 @@ if __name__ == '__main__':
     print("✅ Scheduler started")
 
     # 🔥 RENDER PORT FIX
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
